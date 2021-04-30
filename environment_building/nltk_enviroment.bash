e1f5cbdce75853681361c87dabc1da50d518a6a6
KIT <KIT.RoboCup.HomePJ@gmail.com>
2021-04-30 19:21:34 +0900

    ed 4/30

--- environment_building/nltk_enviroment.bash
+++ environment_building/nltk_enviroment.bash
@@ -1,4 +1,4 @@
-source ../env/bin/activate
+
 wget https://nlp.stanford.edu/software/stanford-tagger-4.2.0.zip
 unzip stanford-tagger-4.2.0.zip
 mv stanford-postagger-full-2020-11-17 ~/catkin_ws/src/voice_common_pkg/config/stanford-postagger
